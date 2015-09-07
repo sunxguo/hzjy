a7aad7eff400f601929ba2a1a0d361787fbb1b57
MonkeyKing <MonkeyKing1024@gmail.com>
2015-09-07 14:56:53 +0800

    update

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '182.92.156.106';
+$cfg_dbname = 'hzjy';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '19910910jacksun';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
